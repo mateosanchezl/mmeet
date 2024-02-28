48207c2a3c86106fb2a76401636a4046deabea35
Mark Graham <markgraham924@gmail.com>
2024-02-28 08:43:09 +0000

    v1.2 Chat Messages
    Included images inside the assets folder
    Changes include:
    - Added left & right sidebar for future content
    - Fixed formatting on messages, and allowed them to be clickable
    - Added username, profile pictures and time of message

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 
-function ChatMessage({ text, image }) {
+function ChatMessage({ text, image, time, user }) {
   return (
-    <div className="chat chat-start">
-      <div className="chat-image avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Avatar"
-            src={image}
-          />
+    <div className="p-1">
+      <button className="w-full cursor-pointer bg-gray-300 hover:bg-gray-200 rounded-lg">
+        <div className="chat chat-end">
+          <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+              <img alt="Avatar" src={image} />
+            </div>
+          </div>
+          <div className="chat-header">
+            {user}
+            <time className="text-xs opacity-50">{time}</time>
+          </div>
+          <div className="chat-bubble w-full p-2">
+            <div>{text}</div>
+          </div>
         </div>
-      </div>
-      <div className="chat-bubble">
-        {text}
-      </div>
+      </button>
     </div>
   );
 }
--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -3,42 +3,90 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ChatMessage from "../components/ChatMessage";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faBell, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChalkboardUser,
+  faPlus,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+
+import profilepic1 from "../assets/profilepics/profilepic1.jpg";
+import profilepic2 from "../assets/profilepics/profilepic2.jpg";
+import profilepic3 from "../assets/profilepics/profilepic3.jpg";
+
+
+
 
 const messages = [
   {
     id: 1,
-    text: "It was said that you would, destroy the Sith, not join them.",
-    image:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
+    text: "This was the original concept for the login screen. After seeing Mateo Sanchez Lopez more modern prototype of the landing page, we decided it best to use that one.",
+    image: profilepic1,
+    time: " 10:15",
+    user: "Kawith Nethaka Samarasekara",
+  },
+  {
+    id: 2,
+    text: "This is the main home screen of the app, inside and contains widgets for quick access,",
+    image: profilepic2,
+    time: " 12:45",
+    user: "Mark Graham",
+  },
+  {
+    id: 3,
+    text: "Brainstorm from introduction up to Sprint one.",
+    image: profilepic3,
+    time: " 04:00",
+    user: "Muhammad Zaid Razzaq Saira",
   },
 ];
 
 function Message() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="wrapper min-h-screen flex flex-col">
       <Navbar />
-      <div className="flex justify-center items-center p-4">
-        <button className="text-gray-600 hover:text-gray-900 border border-black bg-gray-800 hover:bg-blue-600 rounded-lg p-2">
-          <FontAwesomeIcon icon={faPlus} className="text-white text-lg" />
-        </button>
-        <button className="ml-4 text-gray-600 hover:text-gray-900 border border-black bg-gray-800 hover:bg-blue-600 rounded-lg p-2">
-          <FontAwesomeIcon icon={faBell} className="text-white text-lg" />
-        </button>
-        <button className="ml-4 text-gray-600 hover:text-gray-900 border border-black bg-gray-800 hover:bg-blue-600 rounded-lg p-2">
-          <FontAwesomeIcon icon={faSearch} className="text-white text-lg" />
-        </button>
+      <div className="flex-1 flex min-h-0">
+        {/* Left Sidebar */}
+        <div className="w-1/6 bg-gray-200">
+          {/* Left sidebar content goes here */}
+          <p>Left Side Bar Content</p>
+        </div>
+
+        {/* Main Content */}
+        <div className="flex flex-col flex-1">
+          <div className="flex justify-center items-center p-4">
+            <button className="text-gray-600 hover:text-gray-900 border border-black bg-gray-800 hover:bg-blue-600 rounded-lg p-2">
+              <FontAwesomeIcon
+                icon={faChalkboardUser}
+                className="text-white text-lg"
+              />
+            </button>
+            <button className="ml-4 text-gray-600 hover:text-gray-900 border border-black bg-gray-800 hover:bg-blue-600 rounded-lg p-2">
+              <FontAwesomeIcon icon={faPlus} className="text-white text-lg" />
+            </button>
+            <button className="ml-4 text-gray-600 hover:text-gray-900 border border-black bg-gray-800 hover:bg-blue-600 rounded-lg p-2">
+              <FontAwesomeIcon icon={faSearch} className="text-white text-lg" />
+            </button>
+          </div>
+          <div className="p-4">
+            {messages.map((message) => (
+              <ChatMessage
+                key={message.id}
+                text={message.text}
+                image={message.image}
+                time={message.time}
+                user={message.user}
+              />
+            ))}
+          </div>
+          <div className="flex-grow"></div>
+        </div>
+
+        {/* Right Sidebar */}
+        <div className="w-1/6 bg-gray-200">
+          {/* Right sidebar content goes here */}
+          <p>Right Side Bar Content</p>
+        </div>
       </div>
-      <div className="p-4">
-        {messages.map((message) => (
-          <ChatMessage
-            key={message.id}
-            text={message.text}
-            image={message.image}
-          />
-        ))}
-      </div>
-      <div className="flex-grow"></div>
       <Footer />
     </div>
   );
